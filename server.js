807ee7e3a63b84f3ac4e9bb67e461eb81772c308
Philipp Timmalog <ptimmalog@gmail.com>
2022-07-26 12:17:45 +0200

    add node express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var path = require("path");
-var serveStatic = require("serve-static");
-app = express();
-app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log("server started " + port);
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, "dist")))
+  .get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./dist/"));
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
